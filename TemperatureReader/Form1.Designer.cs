3562f36efd6566e11800ef1905d08e396a348a45
Jason Higgins <jason.higgins11@gmail.com>
2009-10-14 01:49:51 -0700

    First
    Signed-off-by: Jason Higgins <jason.higgins11@gmail.com>

---
+++ TemperatureReader/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace TemperatureReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.txtTemperature = new System.Windows.Forms.TextBox();
+            this.txtRouterTemp = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 19200;
+            this.serialPort1.PortName = "COM4";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // txtTemperature
+            // 
+            this.txtTemperature.Location = new System.Drawing.Point(78, 22);
+            this.txtTemperature.Name = "txtTemperature";
+            this.txtTemperature.Size = new System.Drawing.Size(100, 20);
+            this.txtTemperature.TabIndex = 0;
+            // 
+            // txtRouterTemp
+            // 
+            this.txtRouterTemp.Location = new System.Drawing.Point(78, 48);
+            this.txtRouterTemp.Name = "txtRouterTemp";
+            this.txtRouterTemp.Size = new System.Drawing.Size(100, 20);
+            this.txtRouterTemp.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Coordinator";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Router 1";
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.EditButtons = System.Windows.Forms.MouseButtons.Left;
+            this.zedGraphControl1.Location = new System.Drawing.Point(15, 107);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.PanModifierKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Shift | System.Windows.Forms.Keys.None)));
+            this.zedGraphControl1.ScrollGrace = 0;
+            this.zedGraphControl1.ScrollMaxX = 0;
+            this.zedGraphControl1.ScrollMaxY = 0;
+            this.zedGraphControl1.ScrollMaxY2 = 0;
+            this.zedGraphControl1.ScrollMinX = 0;
+            this.zedGraphControl1.ScrollMinY = 0;
+            this.zedGraphControl1.ScrollMinY2 = 0;
+            this.zedGraphControl1.Size = new System.Drawing.Size(547, 325);
+            this.zedGraphControl1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(589, 455);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtRouterTemp);
+            this.Controls.Add(this.txtTemperature);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox txtTemperature;
+        private System.Windows.Forms.TextBox txtRouterTemp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+    }
+}
+
---
+++ TemperatureReader/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using ZedGraph;
+
+namespace TemperatureReader
+{
+    public partial class Form1 : Form
+    {
+        int tickStart;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+            InitGraph();
+
+            serialPort1.Open();
+        }
+
+        private void InitGraph()
+        {
+            GraphPane myPane = zedGraphControl1.GraphPane;
+            myPane.Title.Text = "Temperature Data";
+            myPane.XAxis.Title.Text = "Time, Seconds";
+            myPane.YAxis.Title.Text = "Degrees C";
+
+            // Save 1200 points.  At 50 ms sample rate, this is one minute
+            // The RollingPointPairList is an efficient storage class that always
+            // keeps a rolling set of point data without needing to shift any data values
+            RollingPointPairList list = new RollingPointPairList(1200);
+
+            // Initially, a curve is added with no data points (list is empty)
+            // Color is blue, and there will be no symbols
+            LineItem curve = myPane.AddCurve("Degrees", list, Color.Blue, SymbolType.None);
+
+
+            // Just manually control the X axis range so it scrolls continuously
+            // instead of discrete step-sized jumps
+            myPane.XAxis.Scale.Min = 0;
+            myPane.XAxis.Scale.Max = 30;
+            myPane.XAxis.Scale.MinorStep = 1;
+            myPane.XAxis.Scale.MajorStep = 5;
+
+            // Scale the axes
+            zedGraphControl1.AxisChange();
+
+            // Save the beginning time for reference
+            tickStart = Environment.TickCount;
+
+        }
+
+        private void updateGraph(double newValue)
+        {
+            // Make sure that the curvelist has at least one curve
+            if (zedGraphControl1.GraphPane.CurveList.Count <= 0)
+                return;
+
+            // Get the first CurveItem in the graph
+            LineItem curve = zedGraphControl1.GraphPane.CurveList[0] as LineItem;
+            if (curve == null)
+                return;
+
+            // Get the PointPairList
+            IPointListEdit list = curve.Points as IPointListEdit;
+            // If this is null, it means the reference at curve.Points does not
+            // support IPointListEdit, so we won't be able to modify it
+            if (list == null)
+                return;
+
+            // Time is measured in seconds
+            double time = (Environment.TickCount - tickStart) / 1000.0;
+
+            // 3 seconds per cycle
+            list.Add(time, newValue);
+
+            // Keep the X scale at a rolling 30 second interval, with one
+            // major step between the max X value and the end of the axis
+            Scale xScale = zedGraphControl1.GraphPane.XAxis.Scale;
+            if (time > xScale.Max - xScale.MajorStep)
+            {
+                xScale.Max = time + xScale.MajorStep;
+                xScale.Min = xScale.Max - 30.0;
+            }
+
+            // Make sure the Y axis is rescaled to accommodate actual data
+            zedGraphControl1.AxisChange();
+            // Force a redraw
+            zedGraphControl1.Invalidate();
+        }
+
+        private void getCoordinatorTemp()
+        {
+            serialPort1.Write("8");
+
+            Thread.Sleep(200);
+            String message = serialPort1.ReadExisting();
+            message = message.Substring(message.IndexOf(",") + 1, 4);
+            String upper = message.Substring(0, 2);
+            String lower = message.Substring(2, 2);
+            Byte[] wordArray = new byte[2];
+            wordArray[0] = byte.Parse(lower, System.Globalization.NumberStyles.HexNumber);
+            wordArray[1] = byte.Parse(upper, System.Globalization.NumberStyles.HexNumber);
+            Int16 tempInt = BitConverter.ToInt16(wordArray, 0);
+            double tempDouble = (0.0625) * (double)(tempInt >> 3);
+
+            txtTemperature.Text = tempDouble.ToString();
+
+            Console.WriteLine("temperature1: " + tempDouble);
+
+            updateGraph(tempDouble);
+        }
+
+        private void getRouterTemp(int deviceId)
+        {
+            serialPort1.Write("2");
+            Thread.Sleep(200);
+            serialPort1.Write("02");
+            Thread.Sleep(200);
+            serialPort1.Write("0001");
+
+            Thread.Sleep(1000);
+            String message = serialPort1.ReadExisting();
+
+            String message2 = message.Substring(message.Length - 6, 4);
+
+            String upper = message2.Substring(0, 2);
+            String lower = message2.Substring(2, 2);
+            byte[] wordArray = new byte[2];
+            wordArray[0] = byte.Parse(lower, System.Globalization.NumberStyles.HexNumber);
+            wordArray[1] = byte.Parse(upper, System.Globalization.NumberStyles.HexNumber);
+            Int16 tempInt = BitConverter.ToInt16(wordArray, 0);
+            Double tempDouble = (0.0625) * (double)(tempInt >> 3);
+
+            txtRouterTemp.Text = tempDouble.ToString();
+
+            Console.WriteLine("temperature2: " + tempDouble);
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                getCoordinatorTemp();
+
+               
+            }
+            catch (Exception)
+            {
+
+            }
+
+
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            serialPort1.Close();
+        }
+
+         
+    }
+}
